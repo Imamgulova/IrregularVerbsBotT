9117a7579db1511cdd05deb9f25f55e3ce27b4ad
TotskyA <44275652+TotskyA@users.noreply.github.com>
2018-10-28 23:04:22 +0500

    Add files via upload

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using IrregularVerbsBot.Models;
+
+
+
+namespace IrregularVerbsBot.Controllers
+{
+    [Route("api/message/update")]
+    public class MessageController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "What do you mean?";
+        }
+
+
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody]Update update)
+        {
+            if (update == null) return Ok();
+
+            var commands = Bot.Commands;
+            var message = update.Message;
+            var botClient = await Bot.GetBotClientAsync();
+
+            foreach (var command in commands)
+            {
+                if (command.Contains(message))
+                {
+                    await command.Execute(message, botClient);
+                    break;
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ Models/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace IrregularVerbsBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://URL:443/{0}";
+        public static string Name { get; set; } = "<BOT_NAME>";
+        public static string Key { get; set; } = "<BOT_KEY>";
+    }
+}
